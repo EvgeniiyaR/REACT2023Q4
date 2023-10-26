05c02d89a744457cf49e61f9cf769e3b31f6c559
EvgeniiyaR <tulpansik@mail.ru>
2023-10-27 02:08:06 +0300

    feat: add getArtworksSearch, getArtwork

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://api.artic.edu/api/v1/artworks';
+const BASE_URL = 'https://api.artic.edu/api/v1/artworks/';
 
 const headers = {
   'Content-Type': 'application/json',
@@ -12,7 +12,19 @@ const handleResponse = (res: Response) => {
 };
 
 export const getArtworks = () => {
-  return fetch(BASE_URL, {
+  return fetch(`${BASE_URL}?limit=20`, {
+    headers,
+  }).then((res) => handleResponse(res));
+};
+
+export const getArtworksSearch = (search: string) => {
+  return fetch(`${BASE_URL}search?q=${search}&limit=20`, {
+    headers,
+  }).then((res) => handleResponse(res));
+};
+
+export const getArtwork = (link: string) => {
+  return fetch(link, {
     headers,
   }).then((res) => handleResponse(res));
 };
