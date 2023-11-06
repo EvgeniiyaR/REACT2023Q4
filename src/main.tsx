f4d4882bb654cfd5d280887cad158249cc400302
EvgeniiyaR <tulpansik@mail.ru>
2023-11-06 04:33:21 +0300

    fix: delete App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import { RouterProvider } from 'react-router-dom';
@@ -10,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<div>Error occurred. Please try again.</div>}>
       <RouterProvider router={router} />
-      <App />
     </ErrorBoundary>
   </React.StrictMode>
 );
