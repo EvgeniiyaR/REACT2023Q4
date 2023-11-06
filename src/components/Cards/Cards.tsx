18603216b7798df3a24868af2e9a34acbc4fdb55
EvgeniiyaR <tulpansik@mail.ru>
2023-11-06 04:33:47 +0300

    feat: add props setId

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -4,14 +4,15 @@ import Card from '../Card/Card';
 
 interface ICardsProps {
   data: IArtwork[];
+  setId: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Cards = ({ data }: ICardsProps) => {
+const Cards = ({ data, setId }: ICardsProps) => {
   return (
     <ul className="cards">
       {data.map((item: IArtwork) => (
         <li className="cards__item" key={item.id}>
-          <Card item={item} />
+          <Card item={item} setId={setId} />
         </li>
       ))}
     </ul>
